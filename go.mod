81c6dd7de8414838cb6e0652e809e7767f9eb7d6
line_east <Rodjergame@gmail.com>
2022-03-09 00:15:04 +0300

    initial

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/go-asphyxia/http
+
+go 1.17
---
+++ methods/methods.go
@@ -0,0 +1,14 @@
+package methods
+
+const (
+	OPTIONS = "OPTIONS"
+	GET     = "GET"
+	HEAD    = "HEAD"
+	POST    = "POST"
+	PUT     = "PUT"
+	PATH    = "PATH"
+	DELETE  = "DELETE"
+	TRACE   = "TRACE"
+	LINK    = "LINK"
+	UNLINK  = "UNLINK"
+)
